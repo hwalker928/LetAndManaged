66fc8faeb0f948f4e20013b78c51fd35708fc883
hwalker928 <mail@harrydev.org>
2021-03-24 16:24:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = class LetAndManaged extends Plugin {
   }
   
   async addquote() {
-    const data = await get(`https://LetAndManaged.18walkerh.repl.co/?quote={args.join(' ')}&apikey=harry`);
+    const data = await get(`https://LetAndManaged.18walkerh.repl.co/?quote=${args.join(' ')}&apikey=harry`);
     return {
       send: false,
       result: `${data.body}`
